ede31b02f856e2ec50c4ed154243ea8a264c2714
20Lush <zachfsx83@gmail.com>
2023-03-25 17:12:48 -0700

    first commit

---
+++ build/CMakeFiles/ArrayPublisherExample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ArrayPublisherExample.dir/src/arrayPublisher_example.cpp.o"
+  "ArrayPublisherExample.pdb"
+  "ArrayPublisherExample"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ArrayPublisherExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/zach/ros2_ws/src/red_detect
+# Build directory: /home/zach/ros2_ws/src/red_detect/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
