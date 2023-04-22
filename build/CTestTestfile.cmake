dd109ad5128e2daa9267b687fcd60b6149cbc77e
20Lush <57083746+20Lush@users.noreply.github.com>
2023-04-22 15:28:01 -0700

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zach/ros2_ws/src/red_detect
-# Build directory: /home/zach/ros2_ws/src/red_detect/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
